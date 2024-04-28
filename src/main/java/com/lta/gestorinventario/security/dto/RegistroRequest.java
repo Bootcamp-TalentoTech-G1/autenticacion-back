b1ac92ea05595b051dc80197647a0a6ff92ae24c
Ricardo Mejía <67653677+ramtako8922@users.noreply.github.com>
2024-04-28 04:54:48 -0500

    resto del back

---
+++ src/main/java/com/lta/gestorinventario/security/dto/RegistroRequest.java
@@ -0,0 +1,15 @@
+package com.lta.gestorinventario.security.dto;
+
+import com.lta.gestorinventario.enums.RolEnum;
+import lombok.Data;
+
+import java.util.Set;
+
+@Data
+public class RegistroRequest {
+
+    private String username;
+    private String password;
+    private Set<RolEnum> roles;
+
+}
